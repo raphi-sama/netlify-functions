55f8875393ade5e5062fae989b5692821604cb78
Raphi Stein <rstein@samasource.org>
2021-03-09 16:39:25 -0500

    cors headers

--- netlify/functions/redirect-test.js
+++ netlify/functions/redirect-test.js
@@ -1,7 +1,14 @@
 exports.handler = async (event, context) => {
 
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+  };
+
   return {
     statusCode: 200,
+    headers,
     body: 'Works'
   }
 }
